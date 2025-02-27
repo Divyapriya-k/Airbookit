04726a9a78411ecf6b3c41084d18f466e4cf3d38
Divyapriya-K <diyapriya0428@gmail.com>
2025-02-27 09:41:05 +0530

    Updated

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./Database/db.config.js";
-import userRoute from "./Routers/user.router.js"
+import connectDb from "./Database/dbConfig.js";
+import authRoute from "./Routers/authRouter.js"
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.use(cors());
-
-connectDB();
+connectDb();
 
 app.get("/",(req,res)=>{
-    res.status(200).send("WELCOME TO OUR API")
+  res.status(200).send("Welcome to our api")
 })
 
-app.use("/api/auth",userRoute)
+app.use("/api/auth",authRoute)
 
-const port= process.env.port || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port,()=>{
-    console.log("server started and running on the port");
+    console.log("server started");
     
 })
